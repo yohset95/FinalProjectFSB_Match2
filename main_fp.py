dc70fb10cea0d4dc328a72ed1c7fd301a502d593
Yohanes Setiawan <yoh.set95@gmail.com>
2023-02-25 18:38:59 +0700

    Update main_fp.py

--- main_fp.py
+++ main_fp.py
@@ -18,16 +18,19 @@ if file_upload is not None:
     st.image(display_image)
 
 bt_predict = st.button('Predict!')
-if bt_predict:
-    img = image.load_img(file_upload, target_size=(150, 150))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    images = np.vstack([x])
-    classes = new_model.predict(images, batch_size=10)
-    if classes[0,0] != 0:
-        st.text("Be careful! You are using incorrect mask.")
-    elif classes[0,1] != 0:
-        st.text("You are wearing correct mask! Stay healthy :)")
-    else:
-        st.text("You are not wearing mask!")
+try:
+    if bt_predict:
+        img = image.load_img(file_upload, target_size=(150, 150))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        images = np.vstack([x])
+        classes = new_model.predict(images, batch_size=10)
+        if classes[0,0] != 0:
+            st.text("Be careful! You are using incorrect mask.")
+        elif classes[0,1] != 0:
+            st.text("You are wearing correct mask! Stay healthy :)")
+        else:
+            st.text("You are not wearing mask!")
+except:
+    st.text("Insert image first, please!")
         
